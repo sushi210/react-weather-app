4e4526cfb8b9e61badfa88a3d966deb5ad96fd41
mjmono <markjoshuadedios@gmail.com>
2025-12-16 15:24:42 +0800

    Feat: Add GitHub social link button

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import WeatherContainer from "./containers/WeatherContainer/WeatherContainer";
+import Socials from "./components/Socials/Socials";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <WeatherContainer />
+      <Socials />
     </div>
   );
 }
